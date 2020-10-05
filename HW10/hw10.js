bb1b52ea51c64efcbeb8b7568691017b25609a07
Vladimir-od <58466406+Vladimir-od@users.noreply.github.com>
2020-10-05 08:01:05 +0300

    Add files via upload

--- HW10/hw10.js
+++ HW10/hw10.js
@@ -1,34 +1,32 @@
 "use strict";
-let buttons = document.querySelectorAll("button");
-let buttonClear = buttons[1];
-let buttonAddLink = buttons[0];
-let ol = document.createElement("ol");
-let list = document.querySelector("list");
-list.appendChild(ol);
+const buttons = document.querySelectorAll("button");
+const buttonClear = buttons[1];
+const buttonAddLink = buttons[0];
+const ol = document.createElement("ol");
+const div = document.querySelector("div");
+div.appendChild(ol);
 buttonAddLink.addEventListener("click", createList);
 buttonClear.addEventListener("click", removeAll);
 function createList() {
-    let inputData = prompt("Type link" || "");
+    const inputData = prompt("Type link" || "");
     if (inputData == null || inputData == "" || !inputData.trim()) return;
-    let li = document.createElement("li");
+    const li = document.createElement("li");
     ol.appendChild(li);
 
-    let btn = document.createElement("button");
+    const btn = document.createElement("button");
     btn.textContent = " Delete Link";
     li.after(btn);
     btn.addEventListener("click", () => {
-        li.parentNode.removeChild(li);
-        btn.parentNode.removeChild(btn);
+        li.remove();
+        btn.remove();
     });
 
-    let anc = document.createElement("a");
+    const anc = document.createElement("a");
     li.appendChild(anc);
     anc.setAttribute("href", inputData.trim());
     anc.textContent = inputData.trim();
 };
 function removeAll() {
-    let element = document.querySelector("ol");
-    while (element.firstChild) {
-        element.removeChild(element.firstChild);
-    };
+    const element = document.querySelector("ol");
+    element.innerHTML = "";
 };
