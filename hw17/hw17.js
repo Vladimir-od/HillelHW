bc9c215bf45a0523440b971f5d93f3d22e365b52
Vladimir-od <58466406+Vladimir-od@users.noreply.github.com>
2020-11-05 13:05:09 +0200

    Add files via upload

--- hw17/hw17.js
+++ hw17/hw17.js
@@ -1,88 +1,92 @@
 "use strict";
-let url = "https://swapi.dev/api/";
-let peopleURL = "https://swapi.dev/api/people/";
-let planetsURL = "https://swapi.dev/api/planets/";
-let filmsURL = "https://swapi.dev/api/films/";
-let starshipsURL = " https://swapi.dev/api/starships";
-let vehiclesURL = "https://swapi.dev/api/vehicles/";
-let speciesURL = "https://swapi.dev/api/species/";
-let info2 = document.getElementById("info2");
+const url = "https://swapi.dev/api/";
+const listName = document.querySelector(".listName");
+const list = document.querySelector(".list");
+const listDetail = document.querySelector(".listDetail");
 
-let response = fetch(url);
-response.then(function (response) {
-    return response.json()
-})
-    .then(function (json) {
-        for (let key in json) {
-            createBlock(key, "list");
-        };
-        createBlockItems("list")
-    });
 
-function createBlock(data, nameID) {
-    let items = document.createElement("ul");
-    let listItem = document.createElement('li');
-    let btn = document.createElement('button');
-    btn.preventDefault;
-    btn.setAttribute("class", `${nameID}`)
-    let htmlBody = document.getElementById("info");
-    listItem.appendChild(btn);
-    items.appendChild(listItem);
-    btn.textContent = `${data}`;
-    htmlBody.appendChild(items);
-};
+function init() {
+    const response = fetch(url);
+    response.then(function (response) {
+        return response.json()
+    })
+        .then(function (json) {
+            for (const data in json) {
+                const btn = document.createElement('button');
+                const li = document.createElement('li');
+                btn.preventDefault;
+                btn.setAttribute("class", `${data}`)
+                list.appendChild(li);
+                li.appendChild(btn)
+                btn.textContent = `${data}`;
+            };
+            createBlockItems()
+        });
 
-function createBlockItems(btnID) {
-    let infos = document.querySelectorAll(`.${btnID}`);
-    for (let i = 0; i < infos.length; i++) {
-        infos[i].addEventListener("click", function () {
+}
+init();
+
+function createBlockItems() {
+    const btns = document.querySelectorAll("ul.list>li>button");
+    for (let i = 0; i < btns.length; i++) {
+        btns[i].addEventListener("click", function () {
             getRespond(this.textContent)
         })
     }
-}
-function makeURL(urlEnd) {
-    let url = "https://swapi.dev/api/";
-    if (!urlEnd) {
-        return url;
-    }
-    return url + urlEnd;
 };
-
 function getRespond(nameItem) {
-    let url = makeURL(nameItem);
-    let answer = fetch(url)
+    const url = makeURL(nameItem);
+    const answer = fetch(url)
         .then(function (res) {
             return res.json()
         })
         .then(function (data) {
-            for (let names in data.results) {
-                createBlock2(data.results[names].name, `${nameItem}`);
+            for (const names in data.results) {
+                createBlock(data.results[names].name, nameItem);
             };
         });
-    // createBlockItems(nameItem)
     return answer;
-}
-function createBlock2(data, nameID) {
-    let items = document.createElement("ul");
-    let listItem = document.createElement('li');
-    let btn = document.createElement('button');
+};
+function makeURL(urlEnd) {
+    const url = "https://swapi.dev/api/";
+    return url + urlEnd;
+};
+function createBlock(data, itemName) {
+    const btn = document.createElement('button');
+    const li = document.createElement('li');
     btn.preventDefault;
-    btn.setAttribute("class", `${nameID}`)
-    let htmlBody = document.getElementById("info2");
-    listItem.appendChild(btn);
-    items.appendChild(listItem);
+    btn.setAttribute("class", `${itemName}`)
+    listName.appendChild(li);
+    li.appendChild(btn)
     btn.textContent = `${data}`;
-    htmlBody.appendChild(items);
-    htmlBody.style.display = "block"
 };
 
-function getRespondDetail(){
-  
-    let res = fetch(peopleURL).then(function(res){
-        return res.json()
-    })
-    .then(function(data){
-        console.log(data)
-    })
-}
-getRespondDetail()
+createBlockNameDetails();
+
+function createBlockNameDetails() {
+    const btns = document.querySelectorAll("ul.listName");
+    for (let i = 0; i < btns.length; i++) {
+        btns[i].addEventListener("click", getRespondDetails)
+    }
+};
+function getRespondDetails() {
+    let url = "https://swapi.dev/api/people/1/"
+    const answer = fetch(url)
+        .then(function (res) {
+            return res.json()
+        })
+        .then(function (data) {
+            console.log(data)
+            let output = "";
+            for (const key in data) {
+                output += key + ': ' + data[key] + '; </br>';
+            }
+            createBlockDetails(output)
+        });
+    return answer;
+};
+function createBlockDetails(data) {
+    const li = document.createElement('li');
+    li.innerHTML = data;
+    listDetail.appendChild(li);
+};
